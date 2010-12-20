9cddbeea95788c2d78cfade6ad3198269175e58b
Daniel Berger <dberger@globe.gov>
2010-12-20 14:06:47 -0700

    Override the authenticated? method and changed init.rb to a simple require.

--- init.rb
+++ init.rb
@@ -1,12 +1,1 @@
-klass = Cms::Authentication::Model::InstanceMethods
-
-klass.module_eval do
-  # If the 'AUTH_USER' environment variable is set then
-  # the user has successfully authenticated.
-  #
-  # AUTH_USER is set (or not) via rack-auth-kerberos.
-  #
-  def authenticated?(password)
-    ENV['AUTH_USER'] ? true : false
-  end
-end
+require 'cms/authentication/model'
--- lib/cms/authentication/model.rb
+++ lib/cms/authentication/model.rb
@@ -1,12 +1,32 @@
 require 'krb5_auth'
-require 'rack/auth/kerberos'
 
 module Cms
   module Authentication
     module Model
       module InstanceMethods
-        def authenticated?(password)
-          # TODO: Override this
+        def authenticated?(password, realm = nil)
+          krb5 = Krb5Auth::Krb5.new
+          realm ||= krb5.get_default_realm
+          user = user.login + "@#{realm}"
+
+          begin
+            krb.get_init_creds_password(user, password)
+          rescue Krb5Auth::Krb5::Exception => err
+            case err.message
+              when /client not found/i
+                msg = "Invalid userid '#{user}'"
+              when /integrity check failed/i
+                msg = "Invalid password for '#{user}'"
+              else
+                msg = "Error attempting to validate userid and password"
+            end
+
+            if defined? logger
+              logger.error "Krb5Auth::Krb5::Exception: #{err.message}"
+            end
+          ensure
+            krb5.close
+          end
         end
       end
     end
