22b007cb703144b4e2abcf7f163670085c9515da
Daniel Berger <dberger@globe.gov>
2010-12-20 14:34:15 -0700

    Change module hierarchy to use totall different module names rather than existing BCMS module names.

--- lib/cms/authentication/model.rb
+++ lib/cms/authentication/model.rb
@@ -2,31 +2,29 @@ require 'krb5_auth'
 
 module Cms
   module Authentication
-    module Model
-      module InstanceMethods
-        def authenticated?(password, realm = nil)
-          krb5 = Krb5Auth::Krb5.new
-          realm ||= krb5.get_default_realm
-          user = user.login + "@#{realm}"
+    module Kerberos
+      def authenticated?(password, realm = nil)
+        krb5 = Krb5Auth::Krb5.new
+        realm ||= krb5.get_default_realm
+        user = user.login + "@#{realm}"
 
-          begin
-            krb.get_init_creds_password(user, password)
-          rescue Krb5Auth::Krb5::Exception => err
-            case err.message
-              when /client not found/i
-                msg = "Invalid userid '#{user}'"
-              when /integrity check failed/i
-                msg = "Invalid password for '#{user}'"
-              else
-                msg = "Error attempting to validate userid and password"
-            end
-
-            if defined? logger
-              logger.error "Krb5Auth::Krb5::Exception: #{err.message}"
-            end
-          ensure
-            krb5.close
+        begin
+          krb.get_init_creds_password(user, password)
+        rescue Krb5Auth::Krb5::Exception => err
+          case err.message
+            when /client not found/i
+              msg = "Invalid userid '#{user}'"
+            when /integrity check failed/i
+              msg = "Invalid password for '#{user}'"
+            else
+              msg = "Error attempting to validate userid and password"
           end
+
+          if defined? logger
+            logger.error "Krb5Auth::Krb5::Exception: #{err.message}"
+          end
+        ensure
+          krb5.close
         end
       end
     end
